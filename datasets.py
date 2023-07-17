b8855b96c12561d9b57ef95a8d3211cd1863cef1
BRACHEMI MEFTAH Hanene <68550699+hbrachemi@users.noreply.github.com>
2023-07-17 11:39:43 +0200

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,4 +1,6 @@
 from torchvision.datasets import CIFAR10
+import torchvision
+
 
 class CustomCIFAR10(CIFAR10):
     def __init__(self, root, train=True, transform=None, target_transform=None, download=False):
@@ -17,3 +19,20 @@ class CustomCIFAR10(CIFAR10):
         
         return img, target, index
 
+class CustomGTSRB(torchvision.datasets.ImageFolder): 
+
+    def __init__(self, root,transform=None, target_transform=None):
+        super().__init__(root)
+        self.transform = transform
+        self.target_transform = target_transform
+    
+    def __getitem__(self, index):
+        img, target = self.data[index], self.targets[index]
+        
+        if self.transform is not None:
+            img = self.transform(img)
+        
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        
+        return img, target, index
